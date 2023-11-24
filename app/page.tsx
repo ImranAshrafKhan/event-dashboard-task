e9aa41d754dd12923c01db736d432e5b82d7cf62
Imran Khan <114049822+ImranAshrafKhan@users.noreply.github.com>
2023-11-24 11:39:47 +0500

    Added dashboard structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+export default function () {
   return (
-    <main>
-      <div>Main Page</div>
+    <main className="h-full w-full flex md:flex-row flex-col-reverse border-2 border-black border-solid">
+      <div className="md:me-7 md:w-4/6 w-full h-full flex flex-col border-2 border-black border-solid">
+        <div className="mb-5 h-full w-full bg-white ">lol</div>
+
+        <div className="h-2/6 w-full bg-white ">haha</div>
+      </div>
+
+      <div className="md:w-2/6 w-full flex flex-col border-2 border-black border-solid">
+        <div className="mb-4 h-full w-full bg-white ">lol</div>
+
+        <div className="h-3/5 w-full bg-white hidden md:block">haha</div>
+      </div>
     </main>
   );
 }

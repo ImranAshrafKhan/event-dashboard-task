dad4d11a0b9a7c5f0f8cf43bb5582172f6a8c9e1
Imran Khan <114049822+ImranAshrafKhan@users.noreply.github.com>
2023-11-24 18:05:29 +0500

    Added the left side of the dashboard (i.e., Events list and cards)

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,47 @@
+import Card from './ui/dashboard/card';
 import EventsTable from './ui/dashboard/events-table';
+import Image from 'next/image';
+
 export default function () {
   return (
     <main className="h-full w-full flex md:flex-row flex-col-reverse">
       <div className="md:me-7 md:w-4/6 w-full h-full flex flex-col">
-        {/* <div className="mb-5 h-full w-full  border-2 border-black border-solid"> */}
-        <div className="mb-5 h-full w-full">
-          <EventsTable />
+        {/* The left part of dashboard */}
+        <div className="pb-5 h-5/6 w-full overflow-y-hidden">
+          {/* Here we're displaying event list */}
+          <div className="pb-2 flex justify-between">
+            <h1 className="text-lg font-semibold">Events List</h1>
+            <button className="px-2 py-3 bg-white border border-solid border-slate-300 rounded shadow-md ">
+              <Image
+                src="/filter-icon.svg"
+                width={20}
+                height={20}
+                alt="Filter icon"
+              />
+            </button>
+          </div>
+
+          <div className="h-full overflow-y-auto relative">
+            <EventsTable />
+          </div>
         </div>
 
-        <div className="h-2/6 w-full bg-white border-2 border-black border-solid">
-          haha
+        <div className="h-1/6 w-full flex">
+          {/* The overall dashboard stats cards */}
+          <div className="w-4/12">
+            <Card cardTitle="All Events" amount={210000} />
+          </div>
+          <div className="mx-4 w-4/12">
+            <Card cardTitle="This Month" amount={30} />
+          </div>
+          <div className="w-4/12">
+            <Card cardTitle="Favourite Events" amount={25} />
+          </div>
         </div>
       </div>
 
       <div className="md:w-2/6 w-full flex flex-col">
+        {/* Right part of dasboard */}
         <div className="mb-4 h-full w-full bg-white border-2 border-black border-solid">
           lol
         </div>
